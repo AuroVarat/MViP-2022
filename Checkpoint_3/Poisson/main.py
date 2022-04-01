76c3d01d6c2365cfe8c945d461a83c0f88265a16
AuroVarat <aurovaratpatnaik@gmail.com>
2022-04-02 00:13:32 +0100

    magneto

--- Checkpoint_3/Poisson/main.py
+++ Checkpoint_3/Poisson/main.py
@@ -1,78 +1,236 @@
-from os import system
-import matplotlib
-matplotlib.use('TKAgg')
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm as pb
-plt.rcParams["figure.figsize"] = [7.00, 7.00]
-plt.rcParams["figure.autolayout"] = True
+import sys
 
-np.random.seed(10)
-N = 50
-c_N = N//2
-nstep = int(1e+4)+1
+
+r1by6 = 1/6
+res = 20
 dx = 1.
 e0 = 1.
-s = slice(1,N+1)
-r1by6 = 1/6
-#calc
-system = np.zeros((N+2,N+2,N+2))
+u = 1.
+npad = ((1,1), (1,1),(0, 0))
 
-rho = np.zeros((N+2,N+2,N+2))
-rho[c_N,c_N,c_N] = 1
-#rho[c_N+2,c_N+2,c_N+2] = -1
-#rho[c_N-5:c_N+5,c_N-5:c_N+5,c_N-5:c_N+5] = np.random.choice([1,0.5],(10,10,10))
+def create_checkerboard(shape):
+    mask =  np.bool_(np.indices(shape).sum(axis=0) % 2)
+    return mask, ~mask
 
+def update_gauss_seidel():
+        global potential
+        
+        prev_potential = potential.copy()
+        
+        potential = r1by6*(    
+                                np.roll( potential,1,axis=0)+ 
+                                np.roll(potential,-1,axis=0) + 
+                                np.roll(potential,1,axis=1)+ 
+                                np.roll(potential,-1,axis=1)+
+                                np.roll(potential,1,axis=2)+ 
+                                np.roll(potential,-1,axis=2)
+                            + system
+                            
+                        )
 
-    
-def update():
-    global system
-    
-    prev_system = system.copy()
-    
-    system = r1by6*(    
-                        np.pad
-                        ( 
-                            np.roll( prev_system,1,axis=0)[s,s,s]+ 
-                            np.roll(prev_system,-1,axis=0)[s,s,s] + 
-                            np.roll(prev_system,1,axis=1)[s,s,s] + 
-                            np.roll(prev_system,-1,axis=1)[s,s,s] +
-                            np.roll(prev_system,1,axis=2)[s,s,s]+ 
-                            np.roll(prev_system,-1,axis=2)[s,s,s],pad_width=1
-                        )+ rho
+        potential[select_black] = prev_potential[select_black]
+        potential = np.pad(potential[s,s,s], npad, mode='constant', constant_values=0)
+        #potential = np.pad(potential[s,s,s],pad_width=1)
+        temp = potential.copy()
+        
+        
+        
+        potential = r1by6*(    
+                            np.roll( potential,1,axis=0)+ 
+                            np.roll(potential,-1,axis=0) + 
+                            np.roll(potential,1,axis=1)+ 
+                            np.roll(potential,-1,axis=1)+
+                            np.roll(potential,1,axis=2)+ 
+                            np.roll(potential,-1,axis=2)
+                        + system
                         
                     )
- 
+        
+        potential[select_white] = temp[select_white]
+        potential = np.pad(potential[s,s,s], npad, mode='constant', constant_values=0)
+        #potential = np.pad(potential[s,s,s],pad_width=1)
+        
+        return np.sum(np.abs(potential-prev_potential))
+
+def update_sor(w=0):
+        global potential
+        
+        prev_potential = potential.copy()
+        
+        potential = r1by6*(    
+                                np.roll( potential,1,axis=0)+ 
+                                np.roll(potential,-1,axis=0) + 
+                                np.roll(potential,1,axis=1)+ 
+                                np.roll(potential,-1,axis=1)+
+                                np.roll(potential,1,axis=2)+ 
+                                np.roll(potential,-1,axis=2)
+                            + system
+                            
+                        )
+        
+        
+        
+
+        potential *= w
+        potential += prev_potential*(1-w) 
+
+        potential[select_black] = prev_potential[select_black]
+        potential = np.pad(potential[s,s,:], npad, mode='constant', constant_values=0)
+        #potential = np.pad(potential[s,s,s],pad_width=1)
+        
+        temp = potential.copy()
+    
+        
+        
+        potential = r1by6*(    
+                                np.roll( potential,1,axis=0)+ 
+                                np.roll(potential,-1,axis=0) + 
+                                np.roll(potential,1,axis=1)+ 
+                                np.roll(potential,-1,axis=1)+
+                                np.roll(potential,1,axis=2)+ 
+                                np.roll(potential,-1,axis=2)
+                            + system
+                            
+                        )
+        
+        
+    
+        potential *= w
+        
+        potential += prev_potential*(1-w)
+        
+        potential[select_white] = temp[select_white]
+        potential = np.pad(potential[s,s,:], npad, mode='constant', constant_values=0)
+        #potential = np.pad(potential[s,s,s],pad_width=1)
+        
+        return np.sum(np.abs(potential-prev_potential))
+
+def update_jacobian():  
+        global potential  
+        
+        prev_potential = potential.copy()
+
+        potential = r1by6*(    
+                            np.roll( potential,1,axis=0)+ 
+                            np.roll(potential,-1,axis=0) + 
+                            np.roll(potential,1,axis=1)+ 
+                            np.roll(potential,-1,axis=1)+
+                            np.roll(potential,1,axis=2)+ 
+                            np.roll(potential,-1,axis=2)
+                        + system
+                        
+                    )
+        potential = np.pad(potential[s,s,s],pad_width=1)
+
+        
+        return np.sum(np.absolute(potential-prev_potential))
+
 
     
-    return np.sum(np.absolute(system-prev_system))
+def field_calc():
+    Fx = (np.roll(potential,1,axis=1)-np.roll(potential,-1,axis=1))[s,s,c_N].ravel()/2*dx
+    Fy = (np.roll(potential,1,axis=0)-np.roll(potential,-1,axis=0))[s,s,c_N].ravel()/2*dx
+    Fz = (np.roll(potential,1,axis=2)-np.roll(potential,-1,axis=2))[s,s,c_N].ravel()/2*dx
+    return Fx,Fy,Fz
 
 
+def main(N=50,nstep = int(9e+3)+1,mode = "gaussian",field = "magnetic"):
+    global potential, system, c_N, Nb, s, dims, select_black, select_white
+  
+    c_N = N//2
+    Nb = N+2 
+    s = slice(1,N+1)
+    dims = np.full(3,Nb)
+    
+    potential = np.zeros(dims) 
+    reset = np.zeros(dims) 
 
-for sweep in pb(range(nstep)):
-
-    if np.isclose(update(),0,atol=1e-3):
-        print(sweep)
-        break 
-
-
-
-plt.imshow(system[0:N+2,0:N+2,c_N],interpolation='gaussian')
-plt.colorbar()
-plt.show()
+    system = np.zeros(dims)
+    
+    if field =="electric":
+        system[c_N,c_N,c_N] = 1
+    elif field == "magnetic":
+        system[c_N,c_N] = 1
+        
+    select_black, select_white = create_checkerboard(dims)
 
 
+    if mode == "gaussian":
+        #gauss-seidel
+        for sweep in  pb(range(nstep)):
+            
+            error = update_sor(w=1.89)
+            
+            if np.isclose(error,0,atol=1e-3):
+                break 
+            
+        pos_list = np.array(np.meshgrid(np.arange(1,N+1), np.arange(1,N+1))).T.reshape(-1,2)
+        dis_list = np.linalg.norm(pos_list - c_N,axis=1)
+        
+        sys_list = potential[s,c_N,s].ravel()
+      
+        Ex,Ey,_ = field_calc()
 
+        np.savetxt("Checkpoint_3/Poisson/data/{}_2D_{}_output.dat".format(N,field),potential[0:Nb,0:Nb,c_N])
+        np.savetxt("Checkpoint_3/Poisson/data/{}_{}_output.dat".format(N,field),np.c_[pos_list,dis_list,sys_list,Ex,-Ey])
+        
+        plt.imshow(potential[0:Nb,c_N,0:Nb],cmap="Pastel1",interpolation='gaussian')
+        plt.colorbar()
+        plt.show()
+        
+        # plt.scatter(np.log(dis_list),np.log(sys_list))
+        # plt.show()
+        
+    elif mode == 'jacobian':
+     
+        for sweep in  pb(range(nstep)):
+            if np.isclose(update_jacobian(),0,atol=1e-3):
+                break 
 
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# system = system[0:c_N,0:c_N,0:N]
-# x,y,z = (system > 0.001).nonzero()
-# k = system.flatten()
+        plt.imshow(potential[0:Nb,c_N,0:Nb],interpolation='gaussian')
+        plt.colorbar()
+        plt.show()
 
-# k=k[~np.isclose(k,0,atol=1e-3)]
-# print(k[0])
-# p = ax.scatter(x,y,z,c=k,alpha=1)
-# fig.colorbar(p, ax = ax, shrink = 0.5, aspect = 5)
-# plt.show()
-#np.savetxt("Checkpoint_3/Poisson/data/output.dat",system)
+    elif mode == "sor":
+        w_list = np.linspace(1,2,res,endpoint=False)
+        data = np.zeros((res,2))
+    
+        for index in pb(range(res)):
+            
+            potential = reset
+            w_i = w_list[index]
+            
+            for sweep in range(nstep):
+                if np.isclose(update_sor(w=w_i),0,atol=1e-3):
+                    data[index] = w_i,sweep
+                    break 
+        
+                
+            
+
+        # np.savetxt("Checkpoint_3/Poisson/data/output_sor.dat",data)
+        plt.scatter(data[:,0],data[:,1])
+        plt.show()
+        
+        
+
+   
+    
+if __name__ == "__main__":
+
+    if(len(sys.argv) == 5):
+        N =int(sys.argv[1]) 
+        nstep = int(sys.argv[2])+1
+        mode = sys.argv[3] # gaussian / jacobian / sor
+        field= sys.argv[4] # magnetic / electric
+        
+        main(N=N,nstep = nstep,mode = mode,field = field)
+    elif(len(sys.argv) == 1) :
+        print("No input provided. Default values will be used") 
+        main()
+    else:
+        print("Error. Required Usage: N nstep mode field")
+    
--- Checkpoint_3/test.py
+++ Checkpoint_3/test.py
@@ -1,27 +1,60 @@
-import enum
+
 import numpy as np
-w = np.linspace(1,2,21)
-data = np.empty((21,2))
-for i,j in enumerate(w):
-    data[i] = i,j
-print(data)
-N = 6
-a = np.full((N, N),6)
-b = a.copy()
-
-mask_one =  np.ones((N, N),dtype=bool)
-mask_one[1::2,1::2] = False
-mask_one[::2,::2] = False
-a[mask_one]*=1
-b[np.invert(mask_one)] *= 0 
-
-print(a)
-print(b)
-s = slice(2,4)
-print(a[s,s])
+import matplotlib
+matplotlib.use('TKAgg')
+import numpy as np
+import matplotlib.pyplot as plt
+# w = np.linspace(1,2,21)
 # data = np.empty((21,2))
-# l = np.linspace(1,2,21)
-# print(l)
-# for i,j in enumerate(l):
+# for i,j in enumerate(w):
 #     data[i] = i,j
 # print(data)
+# N = 6
+# a = np.full((N, N),6)
+# b = a.copy()
+
+# mask_one =  np.ones((N, N),dtype=bool)
+# mask_one[1::2,1::2] = False
+# mask_one[::2,::2] = False
+# a[mask_one]*=1
+# b[np.invert(mask_one)] *= 0 
+
+# print(a)
+# print(b)
+# s = slice(2,4)
+# print(a[s,s])
+# # data = np.empty((21,2))
+# # l = np.linspace(1,2,21)
+# # print(l)
+# # for i,j in enumerate(l):
+# #     data[i] = i,j
+# # print(data)
+
+# N  = 4
+# c_N = N//2
+# rho = np.zeros((N+2,N+2))
+# rho[c_N,c_N] = 1
+# pos_list = np.array(np.meshgrid(np.arange(0,N), np.arange(0,N))).T.reshape(-1,2)
+# dis_list = np.linalg.norm(pos_list - c_N,axis=1)
+# print(rho)
+# print(rho[0:N,0:N])
+# res = 20
+# w_list = np.linspace(1,2,res,endpoint=False)
+# print(print(w_list))
+
+# def checkerboard(shape):
+    
+#     return np.bool_(np.indices(shape).sum(axis=0) % 2)
+
+
+
+# print(checkerboard((2,2,2)))
+
+a = np.ones((5,5))
+
+# npad is a tuple of (n_before, n_after) for each dimension
+npad = ((0,0), (0,0),(0, 0))
+b = np.pad(a, npad, mode='constant', constant_values=0)
+print(b)
+# potential = np.pad(potential[s,s,s], npad, mode='constant', constant_values=0)
+# #potential = np.pad(potential[s,s,s],pad_width=1)
